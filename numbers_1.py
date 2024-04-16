f973e1ba6cc3185c35a687cf459707baa1b891b7
Decodeine <abionaadesola@yahoo.com>
2024-04-16 18:22:35 +0100

    createda file variables.py, created another numbers_1.py

---
+++ numbers_1.py
@@ -0,0 +1,6 @@
+
+def func (a,b):
+    result= " a number {} and a string '{}'.".format(a,b)
+    return result
+
+print(func(145,"o"))
---
+++ variables.py
@@ -0,0 +1,13 @@
+pi= 22/7
+#check the datatype of this variable
+print(type(pi))
+
+#called for = 4  
+# this is an invalid variable name as python does not allow for spaces in variable names, also the variable name contains reserved keyword "for" used for loops.
+
+principal= 1000 #principal amount
+Rate = 4  #rate of interest
+Time= 3 #time in years
+
+simple_interest = (principal * Rate * Time) / 100
+print("Simple Interest is: ", simple_interest)
